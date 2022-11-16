af49ce767f1b255e1f4c4639560e0edefc175bd4
makkoar <makkoar@gmail.com>
2022-11-17 00:35:44 +0300

    Вынес Menu в отдельную библиотеку ConsoleMenu.dll

--- Usings.cs
+++ Usings.cs
@@ -1,6 +1,6 @@
-﻿global using System;
+﻿global using ConsoleMenu;
+global using System;
 global using System.Collections.Generic;
-global using System.Diagnostics;
 global using System.Linq;
 global using System.Text;
 global using static BoolsAndCows.Data;
